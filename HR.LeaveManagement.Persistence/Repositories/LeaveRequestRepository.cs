14fe2f9edd9c20091baa225797abd6f2aa6464fb
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2024-05-14 00:29:44 +0100

    feat:creation of repositories in progress

---
+++ HR.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
@@ -0,0 +1,34 @@
+﻿using HR.LeaveManagement.Application.Persistence.Contracts;
+using HR.LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistence.Repositories
+{
+    public class LeaveRequestRepository : GenericRepository<LeaveRequest>, ILeaveRequestRepository
+    {
+        private readonly HRLeaveManagementDbContext _dbContext;
+        public LeaveRequestRepository(HRLeaveManagementDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task ChangeApprovalStatus(LeaveRequest leaveRequest, bool? ApprovalStatus)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<LeaveRequest>> GetLeaveRequestsWithDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LeaveRequest> GetLeaveRequestWithDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HR.LeaveManagement.Persistence/Repositories/LeaveTypeRepository.cs
@@ -0,0 +1,19 @@
+﻿using HR.LeaveManagement.Application.Persistence.Contracts;
+using HR.LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistence.Repositories
+{
+    public class LeaveTypeRepository : GenericRepository<LeaveType>, ILeaveTypeRepository
+    {
+        private readonly HRLeaveManagementDbContext _dbContext;
+        public LeaveTypeRepository(HRLeaveManagementDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
